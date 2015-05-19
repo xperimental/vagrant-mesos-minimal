d19f00e7d5dc45759335766bbe09571210d5414c
Robert Jacob <robert.jacob@holidaycheck.com>
2015-05-19 11:15:44 +0200

    Install docker, mesos and marathon.

---
+++ bootstrap.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Install docker
+wget -qO- https://get.docker.com/ | sh
+
+# Setup keys for mesosphere
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
+DISTRO=$(lsb_release -is | tr '[:upper:]' '[:lower:]')
+CODENAME=$(lsb_release -cs)
+
+# Add the repository
+echo "deb http://repos.mesosphere.io/${DISTRO} ${CODENAME} main" | \
+  sudo tee /etc/apt/sources.list.d/mesosphere.list
+sudo apt-get -y update
+
+# Install mesos & marathon
+sudo apt-get -y install mesos marathon
