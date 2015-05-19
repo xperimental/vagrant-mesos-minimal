a8eea08978b2cbba46b47c6ceeb960bf921255eb
Robert Jacob <robert.jacob@holidaycheck.com>
2015-05-19 13:33:56 +0200

    Configure zookeeper and mesos.

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,7 @@
 
 # Install docker
 wget -qO- https://get.docker.com/ | sh
+sudo usermod -aG docker vagrant
 
 # Setup keys for mesosphere
 sudo apt-key adv --keyserver keyserver.ubuntu.com --recv E56151BF
@@ -15,3 +16,12 @@ sudo apt-get -y update
 
 # Install mesos & marathon
 sudo apt-get -y install mesos marathon
+
+# Configure Zookeeper
+sudo service zookeeper stop
+echo 1 | sudo tee /etc/zookeeper/conf/myid
+sudo service zookeeper start
+
+# Configure Mesos
+echo 'docker,mesos' | sudo tee /etc/mesos-slave/containerizers
+echo '5mins' | sudo tee /etc/mesos-slave/executor_registration_timeout
